41f11c468465f45d1bc73a84bcf97c94536c3198
milan <milan@crossovernepal.com>
2014-10-28 13:48:26 +0545

    added profile formType

---
+++ src/CP/Terms/UserBundle/Form/Type/ProfileFormType.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace CP\Terms\DemoBundle\Form\Type;
+
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class RegistrationFormType extends BaseType
+{
+    protected $class;
+
+    /**
+     * @param string $class The User class name
+     */
+    public function __construct($class)
+    {
+        $this->class = $class;
+    }
+
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => $this->class,
+            'translation_domain' => 'FOSUserBundle',
+            'intention' => 'registration',
+            'cascade_validation' => true
+        ));
+    }
+
+    /**
+     *  {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add(
+            'username',
+            'text',
+            array(
+                'label' => 'form.username',
+                'attr' => array(
+                    'class' => 'form-control'
+                )
+            )
+        );
+
+        $builder->add(
+            'plainPassword',
+            'repeated',
+            array(
+                'type' => 'password',
+                'first_options' => array('label' => 'form.password','attr' => array('class' => 'form-control')),
+                'second_options' => array('label' => 'form.password_confirmation','attr' => array('class' => 'form-control')),
+                'invalid_message' => 'fos_user.password.mismatch'
+            )
+        );
+
+        $builder->add(
+            'agreement_for_latest_terms',
+            'cp_terms_agreement',
+            array(
+                'label' => false
+            )
+        );
+    }
+
+    public function getName()
+    {
+        return 'admin_user_registration';
+    }
+}
